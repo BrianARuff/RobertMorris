9c1bf9437d1a7478385b1bfcd619613a39382f1d
Brian Ruff <thelonewolf198927@gmail.com>
2018-02-06 02:39:48 -0500

    image uploads

--- app/uploaders/book_image_uploader.rb
+++ app/uploaders/book_image_uploader.rb
@@ -1,11 +1,22 @@
 class BookImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
+   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+
+  if Rails.env.test? or Rails.env.cucumber?
+    CarrierWave.configure do |config|
+      config.storage = :file
+      config.enable_processing = false
+    end
+  end
+
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+      config.enable_processing = false
+    end
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -35,13 +46,17 @@ class BookImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
+  def content_type_whitelist
+    /image\//
+  end
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
+
 end

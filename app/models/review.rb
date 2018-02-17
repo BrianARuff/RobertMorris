18df4aa51ce57186362bda0b4a48d549f222e284
Brian Ruff <thelonewolf198927@gmail.com>
2018-02-16 22:37:07 -0500

    test

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   validates :name, :message, :rating,  presence: true
   validates :message, uniqueness: true
+  validates_numericality_of :rating, less_than_or_equal_to: 5
+  validates_numericality_of :rating, greater_than_or_equal_to: 1
 end
